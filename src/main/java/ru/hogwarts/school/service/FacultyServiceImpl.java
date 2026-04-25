feac0642ab04939044d147e1dc76ec8283700af6
kit <nkrenta@inbox.ru>
2026-04-26 00:48:45 +0300

    hw29 - InfoController added, Logs added, development&production configs added. hw29 is done!

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 
@@ -14,20 +16,25 @@ public class FacultyServiceImpl implements FacultyService {
     private final HashMap<Long, Faculty> facultyMap = new HashMap<>();
     private Long COUNTER = 0L;
 
+    Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
+
     @Override
     public Faculty addFaculty(Faculty faculty) {
         faculty.setId(++COUNTER);
         facultyMap.put(faculty.getId(), faculty);
+        logger.info("was invoked method for create new faculty");
         return faculty;
     }
 
     @Override
     public Faculty findFaculty(Long id) {
+        logger.info("was invoked method for find faculty by id");
         return facultyMap.get(id);
     }
 
     @Override
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("was invoked method for edit faculty");
         if (!facultyMap.containsKey(faculty.getId())) {
             return null;
         }
@@ -37,11 +44,13 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public void deleteFaculty(Long id) {
+        logger.info("was invoked method for delete faculty by id");
         facultyMap.remove(id);
     }
 
     @Override
     public List<Faculty> getFacultiesByColor(String color) {
+        logger.info("was invoked method for get faculties by color");
         return facultyMap.values().stream()
                 .filter(faculty -> faculty.getColor().equals(color))
                 .collect(Collectors.toList());
@@ -49,6 +58,7 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Map<Long, List<Faculty>> getAllFaculties() {
+        logger.info("was invoked method for get all faculties");
         Map<Long, List<Faculty>> result = new HashMap<>();
         for (Faculty faculty : facultyMap.values()) {
             result.computeIfAbsent(faculty.getId(), k -> new java.util.ArrayList<>()).add(faculty);
@@ -58,6 +68,7 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty findFacultyByColorOrName(String color, String name) {
+        logger.info("was invoked method for find faculty by color or name");
         return facultyMap.values().stream()
                 .filter(f -> f.getColor().equalsIgnoreCase(color) || f.getName().equalsIgnoreCase(name))
                 .findFirst()
